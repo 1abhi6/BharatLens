6d2dedb771488107d6e8efce8259b71beca1fb5d
Abhishek Gupta <aiwithabhishek@gmail.com>
2025-10-01 18:58:21 +0530

    Implemented chat api

--- app/services/llm_client.py
+++ app/services/llm_client.py
@@ -1,9 +1,11 @@
 # wrapper for OpenAI API
 
-import openai
+# from autogen_ext.models.openai import OpenAIChatCompletionClient
+# from autogen_agentchat.messages import UserMessage
+from langchain_openai import ChatOpenAI
 from app.core.config import settings
 
-openai.api_key = settings.OPENAI_API_KEY
+OPENAI_API_KEY = settings.OPENAI_API_KEY
 
 
 async def generate_response(messages: list[dict]) -> str:
@@ -11,10 +13,18 @@ async def generate_response(messages: list[dict]) -> str:
     messages: list of dicts like [{"role": "user", "content": "hi"}, ...]
     """
     try:
-        resp = await openai.ChatCompletion.acreate(
+        model_client = ChatOpenAI(
             model="gpt-4o-mini",
-            messages=messages,
+            api_key=OPENAI_API_KEY,
         )
-        return resp.choices[0].message["content"]
+
+        # resp = await model_client.create([UserMessage(content=messages[-1].get("content", ""), source="user")])
+        print(messages)
+        print(type(messages))
+        print(list(messages))
+        resp = model_client.invoke(list(messages))
+
+        return resp.content
+
     except Exception as e:
         return f"Error from LLM: {str(e)}"
